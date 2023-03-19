4661894bbba87399498cc8cd730f94eb85f0096f
Danny Ruda <danny.ruda1997@gmail.com>
2023-03-19 11:40:08 +0100

    creates function to randomly shuffle cards array

--- planing/state-management.tsx
+++ planing/state-management.tsx
@@ -13,24 +13,54 @@
     
     const cardArray = [];
     for(let i = 0; i<12; i++) {
-        const card = {clicked: false};
+        const card = {clicked: i < 7 ? true : false, id: i+1};
         cardArray.push(card);
     }
 
     function randomisedArray(arr) {
         const randArr = [];
-        const clickedCards = randArr.filter((card)=>{return card.clicked});
-        const unclickedCards = randArr.filter((card)=>{return !card.clicked});
+        const clickedCards = arr.filter((card)=>{return card.clicked});
+        const unclickedCards = arr.filter((card)=>{return !card.clicked});
         const cardsOrdered = [];
 
+
         let numOfUnclickedCards = 0;
+         
         if(clickedCards.length < 1) {
             numOfUnclickedCards = 8;
         } else {
-            const rand = 
-            numOfUnclickedCards = Math.floor(Math.random()*clickedCards.length + 1)
+            const unclickedRatio =  unclickedCards.length / arr.length ;
+            numOfUnclickedCards = Math.round(unclickedRatio * 8);
         }
-        
+
+        console.log(numOfUnclickedCards)
+
+        const numOfClickedCards = 8 - numOfUnclickedCards;
+        console.log(numOfClickedCards);
+        let maxRandNum = unclickedCards.length;
+        console.log("maxrandom",maxRandNum)
+        for(let i = 0; i < numOfUnclickedCards ; i++) {
+            const randNum = Math.floor(Math.random()*maxRandNum )
+            console.log(randNum)
+            cardsOrdered.push(unclickedCards.splice(randNum,1)[0]);
+            maxRandNum--;
+        }
+
+        maxRandNum = clickedCards.length;
+
+        for(let i = 0; i < numOfClickedCards ; i++) {
+            const randNum = Math.floor(Math.random()*maxRandNum )
+            cardsOrdered.push(clickedCards.splice(randNum,1)[0]);
+            maxRandNum--;
+        }
+
+        maxRandNum = 8;
+
+        for(let i = 0; i < 8; i++) {
+            const randNum = Math.floor(Math.random()*maxRandNum )
+            randArr.push(cardsOrdered.splice(randNum,1)[0]);
+            maxRandNum--;
+        }
 
         return randArr 
     }
